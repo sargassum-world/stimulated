831f68fe0045f8654205a3eff570caf4b175185d
Ethan Li <lietk12@gmail.com>
2022-03-04 19:25:35 -0800

    Add index.js file to make modules importable

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export CSRFController from './csrf.controller';
+export DefaultScrollableController from './default-scrollable.controller';
+export FormSubmissionController from './form-submission.controller';
+export NavigationLinkController from './navigation-link.controller';
+export NavigationMenuController from './navigation-menu.controller';
+export * as ThemeToggle from './theme-toggle';
+export ThemeController from './theme.controller';
+export TurboCacheController from './turbo-cache.controller';
